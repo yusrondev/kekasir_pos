4330b8f8a6a0968f8ed7ec90bc98eab5709aad6a
Yuz Dev <yusron.dev@gmail.com>
2025-04-07 16:41:17 +0700

    DEV - remove useless codef

--- lib/pages/report/index_report_page.dart
+++ lib/pages/report/index_report_page.dart
@@ -61,21 +61,6 @@ class _IndexReportPageState extends State<IndexReportPage> {
    void _initWebView() {
     controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {},
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {},
-          onHttpError: (HttpResponseError error) {},
-          onWebResourceError: (WebResourceError error) {},
-          onNavigationRequest: (NavigationRequest request) {
-            if (request.url.startsWith('https://www.youtube.com/')) {
-              return NavigationDecision.prevent;
-            }
-            return NavigationDecision.navigate;
-          },
-        ),
-      )
       ..loadRequest(Uri.parse(
           'https://kekasir-core.dewadev.id/report/product/${dataMe?['id'] ?? ''}'));
   }
