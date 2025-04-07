e3126214d26ccf29f98fbecc3523374569ac1328
Yuz Dev <yusron.dev@gmail.com>
2025-04-07 16:13:47 +0700

    DEV - layout report

--- lib/pages/report/index_report_page.dart
+++ lib/pages/report/index_report_page.dart
@@ -2,12 +2,13 @@ import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:gap/gap.dart';
-import 'package:kekasir/apis/api_service.dart';
+import 'package:kekasir/apis/auth_service.dart';
 import 'package:kekasir/components/custom_text_component.dart';
 import 'package:kekasir/models/product.dart';
 import 'package:kekasir/utils/colors.dart';
 import 'package:kekasir/utils/variable.dart';
 import 'package:logger/web.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class IndexReportPage extends StatefulWidget {
   const IndexReportPage({super.key});
@@ -17,31 +18,68 @@ class IndexReportPage extends StatefulWidget {
 }
 
 class _IndexReportPageState extends State<IndexReportPage> {
-  ApiService apiService = ApiService();
+  AuthService authService = AuthService();
   List<Product> products = [];
   List<String> productNames = [];
   TextEditingController keyword = TextEditingController();
-  String? selectedProduct;
+  Map<String, dynamic>? dataMe;
   final dropDownKey = GlobalKey<DropdownSearchState>();
+
+  WebViewController? controller;
 
   @override
   void initState() {
     super.initState();
-    fetchProducts(keyword.text);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    fetchUser();
   }
 
-  Future<void> fetchProducts(String keyword) async {
-    final data = await apiService.fetchProducts(keyword);
+  @override
+  void dispose() {
+    // Kembalikan orientasi ke portrait-only saat keluar dari halaman
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
+
+  Future<void> fetchUser() async {
+    final data = await authService.fetchUser();
     if (mounted) {
       setState(() {
-        products = data;
-
-        // Ambil hanya nama produk
-        productNames = products.map((product) => product.name).toList();
+        dataMe = data;
+        _initWebView(); // Inisialisasi WebView setelah data tersedia
       });
-
-      Logger().d(productNames); // Akan mencetak: ['Crafting', 'testing']
     }
+    Logger().d(dataMe);
+  }
+
+   void _initWebView() {
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(
+          'https://kekasir-core.dewadev.id/report/product/${dataMe?['id'] ?? ''}'));
   }
 
   @override
@@ -66,7 +104,7 @@ class _IndexReportPageState extends State<IndexReportPage> {
           padding: defaultPadding,
           child: Column(
             children: [
-              PageTitle(text: "Laporan $selectedProduct", back: true,),
+              PageTitle(text: "Laporan", back: true,),
               Gap(5),
               const TabBar(
                 labelColor: primaryColor,
@@ -83,32 +121,12 @@ class _IndexReportPageState extends State<IndexReportPage> {
                   padding: EdgeInsets.only(top: 10),
                   children: [
                     SizedBox(
-                      height: MediaQuery.of(context).size.height - 200, // Sesuaikan tinggi agar cukup ruang
+                      height: MediaQuery.of(context).size.height,
                       child: TabBarView(
                         children: [
-                          Column(
-                            children: [
-                              DropdownSearch<String>(
-                                onChanged: (value) {
-                                  setState(() {
-                                    selectedProduct = value;
-                                  });
-                                },
-                                key: dropDownKey,
-                                selectedItem: "Menu",
-                                items: (filter, infiniteScrollProps) => productNames,
-                                decoratorProps: DropDownDecoratorProps(
-                                  decoration: InputDecoration(
-                                    border: InputBorder.none
-                                  ),
-                                ),
-                                popupProps: PopupProps.menu(
-                                  
-                                  showSearchBox: true,
-                                    fit: FlexFit.loose, constraints: BoxConstraints()),
-                              ),
-                            ],
-                          ),
+                          controller != null
+                          ? WebViewWidget(controller: controller!) // Gunakan 'controller!'
+                          : Center(child: CircularProgressIndicator()), // Tampilkan loading jika null
                           Center(child: Icon(Icons.directions_transit, size: 100)),
                         ],
                       ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import file_selector_macos
 import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
